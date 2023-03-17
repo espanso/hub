b2cea990051ade4dd4849f234c6949dd1fb04cf4
8-momo-8 <medo349medo@gmail.com>
2023-03-17 19:52:21 +0000

    add the script used to generate

---
+++ main.py
@@ -0,0 +1,11 @@
+from function import *
+
+
+def main():
+    filtered_words()
+    generate_replacements('filtered_words.txt', 'replacements.txt')
+
+
+# simple but just to be organized    
+if __name__ == '__main__':
+    main()
