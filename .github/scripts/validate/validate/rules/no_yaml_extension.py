6c4865765ff0e5927e2fb3a72bd747f946417151
Auca Coyan <aucacoyan@gmail.com>
2024-04-04 22:03:19 -0300

    :art: format python

--- .github/scripts/validate/validate/rules/no_yaml_extension.py
+++ .github/scripts/validate/validate/rules/no_yaml_extension.py
@@ -1,13 +1,15 @@
-import glob 
+import glob
 import os
-import yaml
 from . import ValidationRule
 
-class NoYAMLExtension(ValidationRule):  
-  def name(self):
-    return "no_yaml_extension"
-  
-  def validate(self, path: str):
-    for version_path in self.get_version_paths(path):
-      for yaml_path in glob.glob(os.path.join(version_path, "*.yaml")):
-        raise Exception(f"Please use '.yml' as extension for your files and not .yaml")
+
+class NoYAMLExtension(ValidationRule):
+    def name(self):
+        return "no_yaml_extension"
+
+    def validate(self, path: str):
+        for version_path in self.get_version_paths(path):
+            for yaml_path in glob.glob(os.path.join(version_path, "*.yaml")):
+                raise Exception(
+                    "Please use '.yml' as extension for your files and not .yaml"
+                )
