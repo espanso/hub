dbab16076b8a750f20c17b966843125158a0387a
Auca Coyan <aucacoyan@gmail.com>
2024-04-04 22:10:18 -0300

    :green_heart: update the CI actions and fix `publish-packages.yml` workflow (#108)
    * :green_heart: work on CI
    
    * :art: format python
    
    * :green_heart: fix missing email in CI git CLI

--- .github/scripts/validate/validate/rules/missing_manifest_fields.py
+++ .github/scripts/validate/validate/rules/missing_manifest_fields.py
@@ -1,16 +1,18 @@
-import re
 from . import ValidationRule
 
 REQUIRED_PROPERTIES = ["name", "title", "description", "version", "author"]
 
-class MissingManifestFields(ValidationRule):  
-  def name(self):
-    return "missing_manifest_fields"
 
-  def validate(self, path: str):
-    for version_path in self.get_version_paths(path):
-      manifest = self.read_manifest(version_path)
+class MissingManifestFields(ValidationRule):
+    def name(self):
+        return "missing_manifest_fields"
 
-      for property in REQUIRED_PROPERTIES:
-        if not property in manifest:
-          raise Exception(f"manifest is missing field: {property} in package: {version_path}. These fields are case-sensitive, so make sure you write them all lowercase.")
+    def validate(self, path: str):
+        for version_path in self.get_version_paths(path):
+            manifest = self.read_manifest(version_path)
+
+            for property in REQUIRED_PROPERTIES:
+                if property not in manifest:
+                    raise Exception(
+                        f"manifest is missing field: {property} in package: {version_path}. These fields are case-sensitive, so make sure you write them all lowercase."
+                    )
